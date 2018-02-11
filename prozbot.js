23abdc68b4ac119a8bed6b2d1a16bdb541e63e3b
drnoir <chrispgodber.com>
2018-02-11 21:54:10 +0000

    Updated call to action for telegram
    working telegram callbacks via API added script to noir vortex site
    added express via npm

--- prozbot.js
+++ prozbot.js
@@ -1,88 +1,122 @@
-var TelegramBot = require( "node-telegram-bot-api" );
-var bot = new TelegramBot( "513604411:AAGIHeqGcvQt-Cm45hQhnLdgDNs6uF8oNs0", { polling: true } );
+//server code
+const express = require("express");
+const path = require("path");
+const TelegramBot = require("node-telegram-bot-api");
+const TOKEN = "513604411:AAGIHeqGcvQt-Cm45hQhnLdgDNs6uF8oNs0";
+const server = express();
+const bot = new TelegramBot(TOKEN, { polling: true } );
+const port = process.env.PORT || 5000;
+const gameName = process.env.assholehunter || 'assholehunter';
+const queries = {};
+const url = 'http://noirvortex.co.uk/assholehunter/Prozbot1.html';
+const app = express();
+console.log("started");
 
-bot.onText( /\/start/, function( msg ) {
+server.use(express.static(path.join(__dirname, 'public')));
 
-    bot.sendMessage(
-  
-        msg.from.id,
-  
-        "Hi <b>" + msg.from.first_name + "</> " + msg.from.last_name + "\nWanna play?!",
-  
-        {
-  
-            parse_mode: "HTML"
-  
-       }
-  
-    );
+// Basic configurations
+app.set('view engine', 'ejs');
 
-    bot.onText( /\/play (.+)/, function( msg, match ) {
+// Tunnel to localhost.
+// This is just for demo purposes.
+// In your application, you will be using a static URL, probably that
+// you paid for. :)
+if (url === '0') {
+  const ngrok = require('ngrok');
+  ngrok.connect(port, function onConnect(error, u) {
+    if (error) throw error;
+    url = u;
+    console.log(`Game tunneled at ${url}`);
+  });
+}
 
-        var fromId = msg.from.id;
-      
-        switch( match[1] ) {
-      
-            case "probotskiBot":
-      
-                bot.sendGame(
-      
-                    fromId,
-      
-                    "probotskiBot",
-      
-                    {
-      
-                        reply_markup: JSON.stringify({
-      
-                            inline_keyboard: [
-      
-                                [ { text: "Play", callback_game: JSON.stringify( { game_short_name: "probotskibot" } ) } ],
-      
-                                [ { text: "Share", url: "https://telegram.me/probotskiBot" } ]
-      
-                            ]
-      
-                        })
-      
-                    }
-      
-                );
-      
-                break;
-      
-            default:
-      
-                bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn’t exist.." );
-      
-        }
-      
-      } );
-  
-  } );
+// Matches /start
+bot.onText(/\/start/, function onPhotoText(msg) {
+  bot.sendGame(msg.chat.id, gameName);
+});
 
-  bot.on( "callback_query", function( cq ) {
+// Handle callback queries
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+  bot.answerCallbackQuery(callbackQuery.id, url, true, { url });
+});
 
-    if ( cq.game_short_name ) {
-  
-        switch( cq.game_short_name ) {
-  
-            case "probotskiBot":
-  
-                bot.answerCallbackQuery( cq.id, undefined, false, { url: "http://www.noirvortex.co.uk/prozbot/assholehunter/Prozbot1.html" } );
-  
-                return;
-  
-        }
-  
-        bot.answerCallbackQuery( cq.id, "Sorry, '" + cq.game_short_name + "' is not available.", true );
-  
-    }
-  
-  } );
+// Render the HTML game
+app.get('/', function requestListener(req, res) {
+  res.sendFile(path.join(__dirname, 'game.html'));
+});
 
-  bot.on( "inline_query", function( iq ) {
+// Bind server to port
+app.listen(port, function listen() {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
 
-    bot.answerInlineQuery( iq.id, [ { type: "game", id: "0", game_short_name: "Probotski" } ] );
-  
-  } );
+
+bot.onText(/\/link (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendDocument(chatId, resp);
+});
+
+bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This is an amusing joke HTML5 game that tests you reaction to a prostitute"));
+
+// bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
+
+// bot.on("callback_query", function (query) {
+//     if (query.game_short_name !== gameName) {
+//       bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+//     } else {
+//       queries[query.id] = query;
+//       let gameurl = "https://noirvortex.co.uk/assholehunter/Prozbot1.html?  id="+query.id;
+//       bot.answerCallbackQuery({
+//         callback_query_id: query.id,
+//         url: gameurl
+//       });
+//       console.log(gameurl);
+//     }
+//   });
+
+//   bot.on("inline_query", function(iq) {
+//     bot.answerInlineQuery(iq.id, [ { type: "game", id: "0", game_short_name: gameName } ] );
+//   });
+
+    // bot.onText( /\/play (.+)/, function( msg, match ) {
+
+    //     switch( match[1] ) {
+      
+    //         case "assholehunter":
+      
+    //             bot.sendGame(
+      
+    //                 fromId,
+      
+    //                 "assholehunter",
+      
+    //                 {
+      
+    //                     reply_markup: JSON.stringify({
+      
+    //                         inline_keyboard: [
+      
+    //                             [ { text: "Play", callback_game: JSON.stringify( { game_short_name: "assholehunter" } ) } ],
+      
+    //                             [ { text: "Share", url: "https://telegram.me/prozbotskiBot" } ]
+      
+    //                         ]
+      
+    //                     })
+      
+    //                 }
+      
+    //             );
+      
+    //             break;
+      
+    //         default:
+      
+    //             bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn’t exist.." );
+      
+    //     }
+      
+    // //   } );
+    // server.listen(port);  
+
